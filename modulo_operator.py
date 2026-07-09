7e9435596c7670d3f48d656af2932f2a1faae25b
dull-scythe <teylor.bean@gmail.com>
2026-07-09 17:43:43 -0400

    initial commit

---
+++ modulo_operator.py
@@ -0,0 +1,13 @@
+'''
+Check if input number is even or odd using modulo operator
+'''
+
+num_to_check = int(input("What is the number you want to check?"))
+
+if num_to_check % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+
