dd5fe6dc64fc252aff6c61d08ce277de5ef02611
dull-scythe <teylor.bean@gmail.com>
2026-07-23 14:18:40 -0400

    Day 4 Project: Rock Paper Scissors. Implemented input for user's choice and assigned rock, paper, scissors ascii art to their respective variables.

---
+++ Day 4 - Randomisation & Python Lists/rock_paper_scissors.py
@@ -0,0 +1,51 @@
+print("Welcome to ROCK PAPER SCISSORS!")
+print("Type 0 for Rock")
+print("Type 1 for Paper")
+print("Type 2 for Scissors")
+
+choice = int(input())
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+if choice == 0:
+    '''
+    print ascii for rock
+    '''
+    print(rock)
+
+elif choice == 1:
+    '''
+    print ascii for paper
+    '''
+    print(paper)
+elif choice == 2:
+    '''
+    print ascii for scissors
+    '''
+    print(scissors)
+else:
+    print("Wrong input. Game over!")
+
